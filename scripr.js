a7bcb2fc71b0fa45da7f574459f74b65a906855c
ziyodevweb <157869779+ziyodevweb@users.noreply.github.com>
2024-02-03 10:01:34 +0500

    Main

---
+++ scripr.js
@@ -0,0 +1,27 @@
+async function getuser() {
+    let follow1 = document.getElementById("follow1")
+    let follow2 = document.getElementById("follow2")
+    let follow3 = document.getElementById("follow3")
+    let follow4 = document.getElementById("follow4")
+    let h1 = document.getElementById("name")
+    let img = document.getElementById("img")
+    let inp = document.getElementById("inp").value
+    let githubinfo = await fetch(`http://api.github.com/users/${inp.toLowerCase()}`)
+    let gitres = await githubinfo.json()
+        // .then(res => res.json())
+        // .then(data => {
+            img.src = gitres.avatar_url
+            h1.innerText = gitres.login
+            follow1.innerText = gitres.followers
+            follow2.innerText = gitres.following
+            follow3.innerText = gitres.public_repos
+            follow4.href = gitres.html_url
+            follow4.innerText = gitres.html_url
+            console.log(gitres);
+        // })
+}
+let btn = document.getElementById("btn")
+
+btn.addEventListener("click",()=>{
+    getuser()
+})
